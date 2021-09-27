b51f7dadb60437987d5255303b1a109d0509bf1d
Sebastián Cárdenas <sebca87@gmail.com>
2021-09-27 08:02:28 -0500

    Se modifica el programa para registrar varios eventos y mostrarlos en una tabla y se agrega la explicación del concepto de función

--- js/registrar.js
+++ js/registrar.js
@@ -1,21 +1,131 @@
 // Eventos
 
-let nombreSalones=["Normal", "Grande"];
-let precioSalones=[1000000, 2000000];
+// let nombreSalones=["Normal", "Grande"];
+// let precioSalones=[1000000, 2000000];
 
-let salonNormal = {"nombre":"Normal", "precio":1000000};
-let salonGrande = {"nombre":"Grande", "precio":2000000};
 
-document.write(`Precio: ${salonNormal.precio}`);
+// document.write(`Precio: ${salonNormal.precio}`);
 
-salonNormal['precio']=800000;
+// salonNormal['precio']=800000;
 
-document.write(`Precio: ${salonNormal.precio}`);
-console.log(salonNormal);
+// document.write(`Precio: ${salonNormal.precio}`);
+// console.log(salonNormal);
 
-let precioMeseros=[150000, 100000];
-let precioComida =[50000, 40000];
+// let precioMeseros=[150000, 100000];
+// let precioComida =[50000, 40000];
 
+// function saludar(nombre){
+//      return `HOLA ${nombre} DESDE FUNCIÓN`;
+// }
+
+const saludar =  (nombre, edad)=>{
+
+    if (edad>=18) {
+        document.write(`Hola ${nombre} mayor de edad...`);
+    ``
+    } else {
+        document.write(`Hola ${nombre} menor de edad...`);        
+    }
+    return 1;
+};
+
+const saludar_abreviado = (nombre)=> `Hola ${nombre}, con función flecha abreviada`;
+
+let mensaje = saludar("JUAN", 25);
+document.write(mensaje);
+
+
+/*
+
+let salonNormal = {"nombre":"Normal", 
+                    "precio":1000000, 
+                    "extra_mesero":150000,
+                    "cantidad_meseros":1, 
+                    "plato":50000
+                };
+let salonGrande = {"nombre":"Grande", 
+                    "precio":2000000, 
+                    "extra_mesero":100000, 
+                    "cantidad_meseros":4,
+                    "plato":40000
+                };
+
+let listaSalones = [salonNormal, salonGrande];
+let listaEventos = [];
+
+let continuar;
+
+do {
+    let evento = {};
+    evento['cliente'] = prompt("Escriba su nombre");
+    evento['documento'] = prompt("Escriba su documento");
+    let salon = parseInt(prompt("Seleccione el salón (1. Normal/2. Grande)"));
+    evento['salon']= listaSalones[salon-1];
+    let meseroExtra = confirm("Desea meseros extras???");
+    if(meseroExtra){
+    evento['meseros'] = parseInt(prompt("Escriba la cantidad de meseros..."));
+    }else{
+        evento['meseros']=0;
+    }
+    let servicioComida = confirm("Desea incluir el servicio de comida???");
+
+    if(servicioComida){
+        evento['platos'] = parseInt(prompt("Escriba la cantidad de platos..."));
+    }else{
+        evento['platos']=0;
+    }
+
+    let valorMeseros = evento.meseros*evento.salon.extra_mesero;
+    let valorComida = evento.platos*evento.salon.plato;
+
+    evento['total'] = valorComida + valorMeseros + evento.salon.precio;
+
+    listaEventos.push(evento);
+
+    continuar = confirm("Registrar un nuevo evento?");
+    
+} while (continuar);
+
+let xhtml = `<table>
+                <thead>
+                    <tr>
+                        <th>CLIENTE</th>
+                        <th>SALON</th>
+                        <th>MESEROS</th>
+                        <th>PLATOS</th>
+                        <th>TOTAL</th>
+                    </tr>
+                </thead>
+                <tbody>`;
+
+for (let evento of listaEventos) {
+    
+    let meseros = parseInt(evento.meseros) + parseInt(evento.salon.cantidad_meseros);
+    xhtml += `<tr>
+                <td>${evento.cliente} - ${evento.documento}</td>
+                <td>${evento.salon.nombre}</td>
+                <td>${meseros}</td>
+                <td>${evento.platos}</td>
+                <td>$${evento.total}</td>
+            </tr>`;
+
+}
+
+xhtml += `</tbody>
+        </table>`;
+
+document.write(xhtml);
+*/
+
+// let evento = {
+//     "cliente":"Juan",
+//     "documento":123456,
+//     "salon": salonNormal,
+//     "meseros":0,
+//     "platos":0,
+//     "descuento":0,
+//     "total":0
+// }
 // for (let salon of precioSalones) {
 //     document.write(`Dato ${salon}`);
 //     document.write("</br>");
